61f4abfcf8e9c36a2affcc65814bd8551518b325
Mai Le Duong <maileduong92@gmail.com>
2020-09-28 11:54:22 +0700

    Create Entities

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 ## Technologies
 - ASP.NET Core 3.1
 - Entity Framework Core 3.1
+## Install package
+- Microsoft.EntityFrameworkCore.SqlServer
+- Microsoft.EntityFrameworkCore.Design
+- Microsoft.EntityFrameworkCore.Tools
 ## How to configure and run
 
 ## How to contribute
---
+++ eShopSolutions.Data/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using eShopSolutions.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolutions.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int SortOrder { get; set; }
+        public bool IsShowOnHome { get; set; }
+        public int? ParentId { get; set; }
+        public Status Status { get; set; }
+    }
+}
